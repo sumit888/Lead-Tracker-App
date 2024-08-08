2117cfe9e1e7340a7487643311643e99740891fe
np03s190038 <thapa.sumit007@gmail.com>
2024-08-08 16:25:51 +0545

    firebase change

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
+// import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
 
-const firebaseConfig = {
-    databaseURL: process.env.DATABASE_URL
+// const firebaseConfig = {
+//     databaseURL: process.env.DATABASE_URL
+// }
+
+//CONFIGURATION-----------------
+
+//Step 1 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js"; //firebase Import Code
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js"; //Step 5 
+
+//Step 2
+const firebaseConfig = { //configuration details for required feature i.e. realtime database
+    databaseURL:   "https://chrome-lead-tracker-extension-default-rtdb.asia-southeast1.firebasedatabase.app/" //STEp 4
+                // STEP-7 Replacing actula URL with process.env.DATABASE_URL
 }
 
-const app = initializeApp(firebaseConfig)
-const database = getDatabase(app)
+// console.log(firebaseConfig.databaseURL)
+
+
+//Step 3
+
+const app = initializeApp(firebaseConfig); // It is using intializeApp function and taking firebaseConfig object to initialize app
+const database = getDatabase(app) //STEP 6
+
+//ACTUAL CODE---------------
+
+const referenceInDb = ref(database, "Leads")
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -32,6 +53,8 @@ deleteBtn.addEventListener("dblclick", function() {
 })
 
 inputBtn.addEventListener("click", function() {
+    push(referenceInDb, inputEl.value)
     console.log(inputEl.value)
     inputEl.value = ""
+    
 })
