85f2018300000290ecd500b0521c37e2186fc4d1
np03s190038 <thapa.sumit007@gmail.com>
2024-08-08 22:59:27 +0545

    lf

--- index.js
+++ index.js
@@ -10,6 +10,7 @@
 //Step 1 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js"; //firebase Import Code
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js"; //Step 5 code after setting up realtime database
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js"; //Step 5 code after setting up realtime database //step 7 add onValue function which helps in fetching the data
 
 //Step 2
 const firebaseConfig = { //configuration details for required feature i.e. realtime database
@@ -24,10 +25,24 @@ const firebaseConfig = { //configuration details for required feature i.e. realt
 
 const app = initializeApp(firebaseConfig); // It is using intializeApp function and taking firebaseConfig object to initialize app
 const database = getDatabase(app) //STEP 6
+const referenceInDb = ref(database, "Leads")
+
+//Step 8 create an onValue function which checks the activitiy and returns data from the database.
+
+onValue(referenceInDb, function(snapshot){
+    const snapshotExist = snapshot.exists()
+
+    if(snapshotExist){
+        const snapshotValues = snapshot.val() // getting values 
+        const leads = Object.values(snapshotValues) //and turning into an array
+        //Use the render function with 'leads' to render the leads in the app
+        render(leads)
+    }
+})
 
 //ACTUAL CODE---------------
 
-const referenceInDb = ref(database, "Leads")
+
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -49,7 +64,8 @@ function render(leads) {
 }
 
 deleteBtn.addEventListener("dblclick", function() {
-    
+    remove(referenceInDb)
+    ulEl.innerHTML = ""
 })
 
 inputBtn.addEventListener("click", function() {
